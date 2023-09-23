037b7bffeca098f84afa2d3c396453a487614ff7
Munkhjin <munkhjin0223@gmail.com>
2023-09-23 17:13:26 +0800

    lesson 13&14

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,42 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import './assets/css/bootstrap.min.css';
+import './assets/css/font-awesome.css';
+import './assets/css/templatemo-hexashop.css';
+import './assets/css/owl-carousel.css';
+import './assets/css/lightbox.css';
+import { useEffect, useState } from 'react';
+import Product from './components/Product';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://dummyjson.com/products?limit=9')
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts(data.products);
+      });
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <section className='section' id='products'>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-lg-12'>
+            <div className='section-heading'>
+              <h2>Our Latest Products</h2>
+              <span>Check out all of our products.</span>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='container'>
+        <div className='row'>
+          {products.map((product) => (
+            <Product product={product} key={product.id} />
+          ))}
+        </div>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more ...</p>
-    </>
+    </section>
   );
 }
 
