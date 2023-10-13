aaf45603befecb255e7c674b1cd4b9b1f3a23163
Munkhjin <munkhjin0223@gmail.com>
2023-10-13 13:52:17 +0800

    client side routing

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './assets/css/templatemo-hexashop.css';
 import './assets/css/owl-carousel.css';
 import './assets/css/lightbox.css';
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import Product from './components/Product';
 
 function App() {
@@ -18,25 +19,36 @@ function App() {
   }, []);
 
   return (
-    <section className='section' id='products'>
-      <div className='container'>
-        <div className='row'>
-          <div className='col-lg-12'>
-            <div className='section-heading'>
-              <h2>Our Latest Products</h2>
-              <span>Check out all of our products.</span>
+    <>
+      <section className='section' id='products'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-lg-12'>
+              <div className='section-heading'>
+                <h2>Our Latest Products</h2>
+                <span>Check out all of our products.</span>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className='container'>
-        <div className='row'>
-          {products.map((product) => (
-            <Product product={product} key={product.id} />
-          ))}
+        <div className='container'>
+          <div className='row'>
+            {products.map((product) => (
+              <Product product={product} key={product.id} />
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <footer>
+        <div className='container'>
+          <ul>
+            <li>
+              <Link to='/about'>Бидний тухай</Link>
+            </li>
+          </ul>
+        </div>
+      </footer>
+    </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import App from './App.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/about',
+    element: <h1>About</h1>,
+  },
+  {
+    path: '*',
+    element: <h1>404 - Not found</h1>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
